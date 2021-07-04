783027aa555a6fad6c01bfe6fc98a8c6bba51859
forestus <guilhermepm164@gmail.com>
2021-07-03 21:25:37 -0300

    v1.0

--- src/routes/class.ts
+++ src/routes/class.ts
@@ -2,14 +2,12 @@ import ClassController from '@controllers/ClassController';
 import { Router } from 'express';
 const router = Router();
 import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
-router.use(ensureAuthenticated);
-router.post('/:id', ClassController.store);
-// find
-// router.get('/', ClassController.findAllByNameLastName);
 router.get('/:moduleId', ClassController.findOneModuleandClass);
-// // update
+
+router.use(ensureAuthenticated);
+
+router.post('/:id', ClassController.store);
 router.put('/:moduleId', ClassController.update);
-// router.patch('/:id', ClassController.updateNickname);
-// // delete
 router.delete('/:id', ClassController.destroy);
+
 export default router;
--- src/routes/module.ts
+++ src/routes/module.ts
@@ -2,14 +2,14 @@ import ModuleController from '@controllers/ModuleController';
 import { Router } from 'express';
 const router = Router();
 import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
+
 router.get('/', ModuleController.findAllModuleandClasses);
-router.use(ensureAuthenticated);
-router.post('/', ModuleController.store);
-// // find
 router.get('/:id', ModuleController.findOneModuleandAllClasses);
-// // update
+
+router.use(ensureAuthenticated);
+
+router.post('/', ModuleController.store);
 router.put('/:id', ModuleController.update);
-// router.patch('/:id', ModuleController.updateNickname);
-// // delete
 router.delete('/:id', ModuleController.destroy);
+
 export default router;
