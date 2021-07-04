c6e12c56b98b78dcffd88427ef9fdf8fa456eaf2
forestus <guilhermepm164@gmail.com>
2021-07-03 21:43:04 -0300

    v1.0

--- src/controllers/ModuleController.ts
+++ src/controllers/ModuleController.ts
@@ -52,7 +52,11 @@ class ModuleController {
         return data;
       })
     );
-
+    data.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
     return response.json(data).status(200);
   }
 
