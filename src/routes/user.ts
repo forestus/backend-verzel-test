26858201c44afb359247dd12dfedab1ee55f1917
forestus <guilhermepm164@gmail.com>
2021-07-04 19:00:13 -0300

    v1.0

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,19 +4,14 @@ const router = Router();
 
 import { ensureMaster } from '@middlewares/ensureMaster';
 
-// auth
 router.post('/login', UserController.login);
-
 router.post('/', UserController.store);
 
 router.use(ensureMaster);
 
-// find
 router.get('/', UserController.findAll);
 router.get('/', UserController.findByName);
-// update
 router.put('/:id', UserController.update);
-// delete
 router.delete('/:id', UserController.destroy);
 
 export default router;
