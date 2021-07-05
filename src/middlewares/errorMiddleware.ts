37b055fa28c47761ed3a99c626d8f329e042b9d9
forestus <guilhermepm164@gmail.com>
2021-07-04 23:27:24 -0300

    v1.0

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -8,24 +8,15 @@ function errorMiddleware(
   response: Response,
   _next: NextFunction
 ) {
-  console.log('err: ' + err.constructor.name);
   if (err instanceof ValidationError) {
     return response.status(400).json({
-      message: err.errors
+      error: err.errors
     });
   }
   if (err instanceof AppError) {
-    if (err.message.name == 'QueryFailedError' && err.message.code == '') {
-      return response.status(409).json({
-        type: err.message.name,
-        message: err.message.message,
-        code: err.message.code
-      });
-    } else {
-      return response.status(err.statusCode).json({
-        error: err.message
-      });
-    }
+    return response.status(err.statusCode).json({
+      error: err.message
+    });
   }
   return response.status(500).json({
     error: `Internal Server Error ${err.message}`
